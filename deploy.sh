615b3ad8fb2e4bd7488100dfe29eb5cc13fc3ca4
Stefan Taubert <stefan.taubert@posteo.de>
2022-02-08 09:45:58 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-deactivate
-python3.8 -m pipenv run cxfreeze \
+prog_name="textgrid-tools"
+cli_path=src/cli.py
+
+mkdir -p ./dist
+
+pipenv run cxfreeze \
   -O \
   --compress \
   --target-dir=dist \
   --bin-includes="libffi.so" \
-  --target-name="textgrid-tools" \
-  src/cli.py
-
-echo "compiled."
-# copy to local apps folder
-mkdir -p /home/mi/apps/textgrid-tools
-cp dist/* -r /home/mi/apps/textgrid-tools
-echo "deployed."
+  --target-name=cli \
+  $cli_path
 
 if [ $1 ]
 then
   cd dist
-  zip textgrid-tools-linux.zip ./ -r
+  zip $prog_name-linux.zip ./ -r
   cd ..
   echo "zipped."
 fi
