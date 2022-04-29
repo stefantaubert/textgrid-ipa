394b95af3ac9b443c3ead42408e45627b9c23665
Stefan Taubert <stefan.taubert@posteo.de>
2022-04-29 11:53:14 +0200

    refactored arpa-to-ipa

--- src/textgrid_utils/tiers/arpa_to_ipa_transcription.py
+++ src/textgrid_utils/tiers/arpa_to_ipa_transcription.py
@@ -1,18 +1,15 @@
 from logging import getLogger
 from typing import Optional, Set
 
-from text_utils import symbols_map_arpa_to_ipa
-from text_utils.string_format import StringFormat
-from text_utils.types import Symbol
+from text_utils.pronunciation.ARPAToIPAMapper import symbol_map_arpa_to_ipa
 from textgrid.textgrid import TextGrid
+
 from textgrid_utils.globals import ExecutionResult
-from textgrid_utils.helper import get_all_intervals, get_mark_symbols
-from textgrid_utils.validation import (InvalidGridError,
-                                       InvalidStringFormatIntervalError,
-                                       NotExistingTierError)
+from textgrid_utils.helper import get_all_intervals, get_mark
+from textgrid_utils.validation import InvalidGridError, NotExistingTierError
 
 
-def map_arpa_to_ipa(grid: TextGrid, tier_names: Set[str], tiers_string_format: StringFormat, replace_unknown: bool, replace_unknown_with: Optional[Symbol], ignore: Set[Symbol]) -> ExecutionResult:
+def map_arpa_to_ipa(grid: TextGrid, tier_names: Set[str], replace_unknown: bool, replace_unknown_with: Optional[str], ignore: Set[str]) -> ExecutionResult:
   assert len(tier_names) > 0
 
   if error := InvalidGridError.validate(grid):
@@ -24,27 +21,25 @@ def map_arpa_to_ipa(grid: TextGrid, tier_names: Set[str], tiers_string_format: S
 
   intervals = list(get_all_intervals(grid, tier_names))
 
-  if error := InvalidStringFormatIntervalError.validate_intervals(intervals, tiers_string_format):
-    return error, False
-
   logger = getLogger(__name__)
 
   changed_anything = False
 
   for interval in intervals:
-    arpa_symbols = get_mark_symbols(interval, tiers_string_format)
-    ipa_symbols = symbols_map_arpa_to_ipa(
-      arpa_symbols=arpa_symbols,
+    arpa_symbol = get_mark(interval)
+    ipa_symbol = symbol_map_arpa_to_ipa(
+      arpa_symbol=arpa_symbol,
       ignore=ignore,
       replace_unknown=replace_unknown,
       replace_unknown_with=replace_unknown_with,
     )
 
-    ipa = tiers_string_format.convert_symbols_to_string(ipa_symbols)
+    if ipa_symbol is None:
+      ipa_symbol = ""
 
-    if interval.mark != ipa:
-      logger.debug(f"Mapped \"{interval.mark}\" to \"{ipa}\".")
-      interval.mark = ipa
+    if arpa_symbol != ipa_symbol:
+      logger.debug(f"Mapped \"{arpa_symbol}\" to \"{ipa_symbol}\".")
+      interval.mark = ipa_symbol
       changed_anything = True
 
   return None, changed_anything
--- src/textgrid_utils_cli/tiers/arpa_to_ipa_transcription.py
+++ src/textgrid_utils_cli/tiers/arpa_to_ipa_transcription.py
@@ -6,27 +6,22 @@ from typing import List, Optional, Set
 from ordered_set import OrderedSet
 from text_utils.string_format import StringFormat
 from text_utils.types import Symbol
+
+from textgrid_utils import map_arpa_to_ipa
 from textgrid_utils_cli.common import process_grids_mp
 from textgrid_utils_cli.globals import ExecutionResult
-from textgrid_utils_cli.helper import (ConvertToOrderedSetAction,
-                                       add_chunksize_argument,
-                                       add_directory_argument,
-                                       add_maxtaskperchild_argument,
-                                       add_n_digits_argument,
-                                       add_n_jobs_argument,
-                                       add_output_directory_argument,
-                                       add_overwrite_argument,
-                                       add_string_format_argument,
-                                       add_tiers_argument, get_optional,
+from textgrid_utils_cli.helper import (ConvertToOrderedSetAction, add_chunksize_argument,
+                                       add_directory_argument, add_maxtaskperchild_argument,
+                                       add_n_digits_argument, add_n_jobs_argument,
+                                       add_output_directory_argument, add_overwrite_argument,
+                                       add_string_format_argument, add_tiers_argument, get_optional,
                                        parse_non_empty)
-from textgrid_utils import map_arpa_to_ipa
 
 
 def get_arpa_to_ipa_transcription_parser(parser: ArgumentParser):
   parser.description = "This command maps ARPA transcriptions to IPA."
   add_directory_argument(parser)
   add_tiers_argument(parser, "tiers which should be transcribed")
-  add_string_format_argument(parser, "tiers")
   parser.add_argument("--replace-unknown", action="store_true",
                       help="replace unknown ARPA symbols with a custom symbol")
   parser.add_argument("--symbol", metavar="SYMBOL", type=get_optional(parse_non_empty),
@@ -42,14 +37,13 @@ def get_arpa_to_ipa_transcription_parser(parser: ArgumentParser):
   return app_map_arpa_to_ipa
 
 
-def app_map_arpa_to_ipa(directory: Path, tiers: OrderedSet[str], formatting: StringFormat, replace_unknown: bool, symbol: Optional[Symbol], ignore: OrderedSet[Symbol], n_digits: int, output_directory: Optional[Path], overwrite: bool, n_jobs: int, chunksize: int, maxtasksperchild: Optional[int]) -> ExecutionResult:
+def app_map_arpa_to_ipa(directory: Path, tiers: OrderedSet[str], replace_unknown: bool, symbol: Optional[Symbol], ignore: OrderedSet[Symbol], n_digits: int, output_directory: Optional[Path], overwrite: bool, n_jobs: int, chunksize: int, maxtasksperchild: Optional[int]) -> ExecutionResult:
   method = partial(
     map_arpa_to_ipa,
     ignore=ignore,
     replace_unknown=replace_unknown,
     replace_unknown_with=symbol,
     tier_names=tiers,
-    tiers_string_format=formatting,
   )
 
   return process_grids_mp(directory, n_digits, output_directory, overwrite, method, chunksize, n_jobs, maxtasksperchild)
