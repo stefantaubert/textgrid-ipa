5959d667620b9a9b8385c9977ba3cb865d509644
Stefan Taubert <stefan.taubert@posteo.de>
2022-04-29 11:32:18 +0200

    refactored symbols-joining

--- src/textgrid_utils/intervals/symbols_joining.py
+++ src/textgrid_utils/intervals/symbols_joining.py
@@ -1,22 +1,14 @@
-from typing import Generator, Iterable, List, Optional, Set, Tuple, cast
+from typing import Generator, List, Set, Tuple, cast
 
 from ordered_set import OrderedSet
-from text_utils import StringFormat, Symbol
-from text_utils.pronunciation.ipa2symb import (merge_fusion_with_ignore,
-                                               merge_left_core,
-                                               merge_right_core)
+from text_utils.pronunciation.ipa2symb import merge_left_core, merge_right_core
 from textgrid.textgrid import Interval, TextGrid
+
 from textgrid_utils.comparison import check_intervals_are_equal
 from textgrid_utils.globals import ExecutionResult
 from textgrid_utils.helper import get_all_tiers, get_mark
-from textgrid_utils.interval_format import IntervalFormat
-from textgrid_utils.intervals.common import (
-    merge_intervals_custom_symbol, replace_intervals)
-from textgrid_utils.validation import (InvalidGridError,
-                                       InvalidStringFormatIntervalError,
-                                       NotExistingTierError,
-                                       NotMatchingIntervalFormatError,
-                                       ValidationError)
+from textgrid_utils.intervals.common import merge_intervals_custom_symbol, replace_intervals
+from textgrid_utils.validation import InvalidGridError, NotExistingTierError, ValidationError
 
 
 class InvalidModeError(ValidationError):
@@ -35,7 +27,7 @@ class InvalidModeError(ValidationError):
     return "Mode needs to be 'right', 'left' or 'together'!"
 
 
-def join_interval_symbols(grid: TextGrid, tier_names: Set[str], tiers_string_format: StringFormat, tiers_interval_format: IntervalFormat, custom_join_symbol: Optional[Symbol], join_symbols: OrderedSet[Symbol], ignore_join_symbols: OrderedSet[Symbol], mode: str) -> ExecutionResult:
+def join_interval_symbols(grid: TextGrid, tier_names: Set[str], join_with: str, join_symbols: OrderedSet[str], ignore_join_symbols: OrderedSet[str], mode: str) -> ExecutionResult:
   assert len(tier_names) > 0
 
   if error := InvalidGridError.validate(grid):
@@ -50,19 +42,11 @@ def join_interval_symbols(grid: TextGrid, tier_names: Set[str], tiers_string_for
 
   tiers = list(get_all_tiers(grid, tier_names))
 
-  for tier in tiers:
-    if error := InvalidStringFormatIntervalError.validate_tier(tier, tiers_string_format):
-      return error, False
-
-    if error := NotMatchingIntervalFormatError.validate_tier(tier, tiers_interval_format, tiers_string_format):
-      return error, False
-
   changed_anything = False
   for tier in tiers:
     intervals_copy = cast(List[Interval], list(tier.intervals))
     for chunk in chunk_intervals(intervals_copy, join_symbols, ignore_join_symbols, mode):
-      merged_interval = merge_intervals_custom_symbol(
-        chunk, tiers_string_format, custom_join_symbol)
+      merged_interval = merge_intervals_custom_symbol(chunk, join_with)
       if not check_intervals_are_equal(chunk, [merged_interval]):
         replace_intervals(tier, chunk, [merged_interval])
         changed_anything = True
@@ -70,7 +54,7 @@ def join_interval_symbols(grid: TextGrid, tier_names: Set[str], tiers_string_for
   return None, changed_anything
 
 
-def chunk_intervals(intervals: List[Interval], join_symbols: Set[Symbol], ignore_join_symbols: Set[Symbol], mode: str) -> Generator[List[Interval], None, None]:
+def chunk_intervals(intervals: List[Interval], join_symbols: Set[str], ignore_join_symbols: Set[str], mode: str) -> Generator[List[Interval], None, None]:
   intervals_as_symbols = tuple(
     get_mark(interval) for interval in intervals
   )
