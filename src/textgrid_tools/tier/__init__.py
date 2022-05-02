5a7238d7c04fda0b1d8493e75a2c118617214256
Stefan Taubert <stefan.taubert@posteo.de>
2022-05-02 12:53:18 +0200

    renamed to exporting

--- src/textgrid_tools/tier/__init__.py
+++ src/textgrid_tools/tier/__init__.py
@@ -1,5 +1,5 @@
 from textgrid_tools.tier.cloning import clone_tier
+from textgrid_tools.tier.exporting import convert_tier_to_text
 from textgrid_tools.tier.mapping import map_tier
 from textgrid_tools.tier.moving import move_tier
 from textgrid_tools.tier.renaming import rename_tier
-from textgrid_tools.tier.text_conversion import convert_tier_to_text
