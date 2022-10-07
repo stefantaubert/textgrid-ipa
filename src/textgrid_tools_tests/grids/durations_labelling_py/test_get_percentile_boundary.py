e363b540a11028becf65972ee1701ecbf803a448
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2022-10-07 15:20:05 +0200

    add tests

--- src/textgrid_tools_tests/grids/durations_labelling_py/test_get_percentile_boundary.py
+++ src/textgrid_tools_tests/grids/durations_labelling_py/test_get_percentile_boundary.py
@@ -31,3 +31,38 @@ def test_same__returns_same():
   res_min, res_max = get_percentile_boundary(durations, 20, 20)
   assert res_min == 2.0
   assert res_max == 2.0
+
+
+def test_uneven_number():
+  durations = np.array([1, 2, 3, 4])
+  res_min, res_max = get_percentile_boundary(durations, 5, 90)
+  assert res_min == 1.15
+  assert res_max == 3.7
+
+
+def test_one_entry_5_90__returns_entry_twice():
+  durations = np.array([3])
+  res_min, res_max = get_percentile_boundary(durations, 5, 90)
+  assert res_min == 3.0
+  assert res_max == 3.0
+
+
+def test_one_entry_0_90__returns_entry_twice():
+  durations = np.array([3])
+  res_min, res_max = get_percentile_boundary(durations, 0, 90)
+  assert res_min == 3.0
+  assert res_max == 3.0
+
+
+def test_one_entry_90_inf__returns_entry_inf():
+  durations = np.array([3])
+  res_min, res_max = get_percentile_boundary(durations, 90, math.inf)
+  assert res_min == 3.0
+  assert res_max == math.inf
+
+
+def test_two_entries__returns_entry_twice():
+  durations = np.array([3, 4])
+  res_min, res_max = get_percentile_boundary(durations, 5, 90)
+  assert res_min == 3.05
+  assert res_max == 3.9
