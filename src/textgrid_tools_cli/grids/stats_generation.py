a239d42d4a9411f49912e20dceefed490e358728
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2022-10-24 17:25:41 +0200

    improvements

--- src/textgrid_tools_cli/grids/stats_generation.py
+++ src/textgrid_tools_cli/grids/stats_generation.py
@@ -16,12 +16,12 @@ from textgrid_tools_cli.validation import FileAlreadyExistsError
 
 
 def get_grids_plot_stats_parser(parser: ArgumentParser):
-  parser.description = "This command creates a violin plot of the all grids."
+  parser.description = "This command creates a violin plot of the all grids and exports marks statistics."
   add_directory_argument(parser)
   parser.add_argument("plot_output", type=parse_path, metavar="PLOT-OUTPUT",
                       help="path to output the generated diagram (*.png or *.pdf)")
   parser.add_argument("marks_output", type=parse_path, metavar="MARKS-OUTPUT",
-                      help="path to output the generated marks csv")
+                      help="path to output the generated marks statistics CSV file")
   add_encoding_argument(parser)
   add_overwrite_argument(parser)
   return app_plot_stats
@@ -49,7 +49,6 @@ def app_plot_stats(ns: Namespace) -> ExecutionResult:
     logger.error(error.default_message)
     return False, False
 
-  everything_successful = True
   grids: List[TextGrid] = []
   for file_nr, (file_stem, rel_path) in enumerate(tqdm(grid_files.items()), start=1):
     flogger.info(f"Processing {file_stem}")
@@ -86,7 +85,7 @@ def app_plot_stats(ns: Namespace) -> ExecutionResult:
   try:
     cast(Path, ns.marks_output).write_text(marks, "UTF-8")
   except Exception as ex:
-    logger.error("Saving of plot was not successful!")
+    logger.error("Saving of marks statistics was not successful!")
     logger.debug(ex)
     return False, True
 
