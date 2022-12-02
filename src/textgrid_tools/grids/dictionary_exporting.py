0b4ac20aaeed6d6d989375a4e50ca11a4b866ff9
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2022-12-02 12:50:42 +0100

    improved validation for pronunciation dictionary creation

--- src/textgrid_tools/grids/dictionary_exporting.py
+++ src/textgrid_tools/grids/dictionary_exporting.py
@@ -3,12 +3,28 @@ from logging import Logger, getLogger
 from typing import Dict, Iterable, List, Literal, Optional, Set, Tuple, Union
 
 from pronunciation_dictionary import PronunciationDict
-from textgrid import TextGrid
+from textgrid import Interval, TextGrid
 
-from textgrid_tools.helper import (get_boundary_timepoints_from_intervals, get_intervals_on_tier,
-                                   get_single_tier, ignore_intervals_by_mark)
+from textgrid_tools.helper import (get_boundary_timepoints_from_intervals, get_interval_readable,
+                                   get_intervals_on_tier, get_single_tier, ignore_intervals_by_mark)
 from textgrid_tools.validation import (BoundaryError, InvalidGridError, NonDistinctTiersError,
                                        NotExistingTierError, ValidationError)
+
+
+class IntervalContainsSpaceError(ValidationError):
+  def __init__(self, interval: Interval) -> None:
+    super().__init__()
+    self.interval = interval
+
+  @classmethod
+  def validate(cls, interval: Interval):
+    if " " in interval.mark:
+      return cls(interval)
+    return None
+
+  @property
+  def default_message(self) -> str:
+    return f"Interval contains space!\n{get_interval_readable(self.interval)}"
 
 
 def create_dictionaries(grids: Dict[str, List[TextGrid]], words_tier_name: str, pronunciations_tier_name: str, scope: Optional[Literal["folder", "all"]], ignore_marks: Set[str], logger: Optional[Logger]) -> Tuple[Optional[ValidationError], Union[PronunciationDict, Dict[str, PronunciationDict]]]:
@@ -39,8 +55,14 @@ def create_dictionaries(grids: Dict[str, List[TextGrid]], words_tier_name: str, 
         return error, False
 
       for word_interval in words_tier_intervals:
+        if error := IntervalContainsSpaceError.validate(word_interval):
+          return error, False
+
         word = word_interval.mark
         pronunciation_intervals = get_intervals_on_tier(word_interval, pronunciation_tier)
+        for interval in pronunciation_intervals:
+          if error := IntervalContainsSpaceError.validate(interval):
+            return error, False
         pronunciation = tuple(interval.mark for interval in pronunciation_intervals)
         speaker_pronunciations.append((word, pronunciation))
     speakers_pronunciations[speaker_name] = speaker_pronunciations
@@ -62,7 +84,8 @@ def get_dict_from_pronunciations(pronunciations: Iterable[Tuple[str, Tuple[str, 
   dictionary: PronunciationDict = OrderedDict()
   weights = Counter(pronunciations)
   # sort after word and then descending after weight and then after pronunciation
-  sorted_pronunciations = sorted(weights.items(), key=lambda wp_w: (wp_w[0][0], -wp_w[1], wp_w[0][1]))
+  sorted_pronunciations = sorted(
+    weights.items(), key=lambda wp_w: (wp_w[0][0], -wp_w[1], wp_w[0][1]))
   for (word, pronunciation), weight in sorted_pronunciations:
     if word not in dictionary:
       dictionary[word] = OrderedDict()
